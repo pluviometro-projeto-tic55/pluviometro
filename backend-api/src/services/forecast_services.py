92d9fd68e21ed36d85f070bac201159a58e634bb
annabeatriceneumann <annabeatriceneumann@gmail.com>
2026-05-24 19:38:27 -0300

    Update timestamp handling to include timezone
    Fix timezone and timestamp update

--- backend-api/src/services/forecast_services.py
+++ backend-api/src/services/forecast_services.py
@@ -236,7 +236,9 @@ def get_station_data_by_id(rc_id):
     response_details = {
         "rdID": data_details.rdID,
         "rcID": data_details.rcID,
-        "timestamp": data_details.timestamp.isoformat(),
+        "timestamp": data_details.timestamp.replace(
+    tzinfo=timezone.utc
+).astimezone().isoformat(),
         "last_update": int(
     (datetime.now(timezone.utc) - data_details.timestamp.replace(tzinfo=timezone.utc))
     .total_seconds() / 60
